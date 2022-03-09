2ce6328171e368e27908046e1a32e6db81098936
Marlon <Marlonnlmx@outlook.com>
2022-03-09 08:43:09 -0300

    Roadmap section finished

---
+++ src/components/DrawSvg.js
@@ -0,0 +1,96 @@
+import React from "react";
+import styled from "styled-components";
+import Vector from "../Icons/Vector";
+import { useRef } from "react";
+import gsap from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
+import { useLayoutEffect } from "react";
+import { keyframes } from "styled-components";
+
+const VectorContainer = styled.div`
+  position: absolute;
+  top: 0.5rem;
+  left: 50%;
+  transform: translateX(-50%);
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+
+  svg {
+    width: 100%;
+    height: 100%;
+  }
+`;
+
+const Bounce = keyframes`
+  from { transform: translateX(-50%) scale(0.5); }
+  to { transform: translateX(-50%) scale(1); }
+`;
+
+const Ball = styled.div`
+  position: absolute;
+  top: 0;
+  left: 50%;
+  transform: translateX(-50%);
+  width: 1.5rem;
+  height: 1.5rem;
+  border-radius: 50%;
+  background-color: ${(props) => props.theme.text};
+  animation: ${Bounce} 0.5s linear infinite alternate;
+`;
+
+const DrawSvg = () => {
+  const ref = useRef(null);
+  const ballRef = useRef(null);
+
+  gsap.registerPlugin(ScrollTrigger);
+  useLayoutEffect(() => {
+    let element = ref.current;
+
+    let svg = document.getElementsByClassName("svg-path")[0];
+
+    const length = svg.getTotalLength();
+
+    // Start positioning of svg drawing
+    svg.style.strokeDasharray = length;
+
+    // Hide svg before scrolling start
+    svg.style.strokeDashoffset = length;
+
+    let t1 = gsap.timeline({
+      scrollTrigger: {
+        trigger: element,
+        start: "top center",
+        end: "bottom bottom",
+        onUpdate: (self) => {
+          const draw = length * self.progress;
+
+          // also reverse the drawing when scroll goes up
+          svg.style.strokeDashoffset = length - draw;
+        },
+        onToggle: (self) => {
+          if (self.isActive) {
+            ballRef.current.style.display = "none";
+          } else {
+            ballRef.current.style.display = "inline-block";
+          }
+        },
+      },
+    });
+
+    return () => {
+      if (t1) t1.kill();
+    };
+  }, []);
+
+  return (
+    <>
+      <Ball ref={ballRef} />
+      <VectorContainer ref={ref}>
+        <Vector />
+      </VectorContainer>
+    </>
+  );
+};
+
+export default DrawSvg;
--- src/components/sections/Roadmap.js
+++ src/components/sections/Roadmap.js
@@ -1,7 +1,202 @@
-import React from "react";
+import gsap from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
+import React, { useLayoutEffect, useRef } from "react";
+import styled from "styled-components";
+import DrawSvg from "../DrawSvg";
+
+const Section = styled.section`
+  min-height: 100vh;
+  width: 100vw;
+  background-color: ${(props) => props.theme.body};
+  position: relative;
+`;
+
+const Title = styled.h1`
+  font-size: ${(props) => props.theme.fontxxl};
+  text-transformation: capitalize;
+  color: ${(props) => props.theme.text};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 1rem auto;
+  border-bottom: 2px solid ${(props) => props.theme.text};
+  width: fit-content;
+`;
+
+const Container = styled.div`
+  width: 70%;
+  height: 200vh;
+  background-color: ${(props) => props.theme.body};
+  margin: 0 auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+`;
+
+const SvgContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Items = styled.ul`
+  list-style: none;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  & > *:nth-of-type(2n + 1) {
+    justify-content: start;
+
+    div {
+      border-radius: 50px 0 50px 0;
+    }
+    p {
+      border-radius: 40px 0 40px 0;
+    }
+  }
+  & > *:nth-of-type(2n) {
+    justify-content: end;
+
+    div {
+      border-radius: 0 50px 0 50px;
+      text-align: left;
+    }
+    p {
+      border-radius: 0 40px 0 40px;
+    }
+  }
+`;
+const Item = styled.li`
+  width: 100%;
+  height: 100%;
+  display: flex;
+`;
+
+const ItemContainer = styled.div`
+  width: 40%;
+  height: fit-content;
+  padding: 1rem;
+  border: 3px solid ${(props) => props.theme.text};
+`;
+
+const Box = styled.p`
+  height: fit-content;
+  background-color: ${(props) => props.theme.carouselColor};
+  color: ${(props) => props.theme.text};
+  padding: 1rem;
+  position: relative;
+  border: 1px solid ${(props) => props.theme.text};
+`;
+
+const SubTitle = styled.span`
+  display: block;
+  font-size: ${(props) => props.theme.fontxl};
+  text-transform: capitalize;
+  color: ${(props) => props.theme.text};
+`;
+const Text = styled.span`
+  display: block;
+  font-size: ${(props) => props.theme.fontsm};
+  text-transform: capitalize;
+  color: ${(props) => props.theme.text};
+
+  font-weight: 400;
+  margin: 0.5rem 0;
+`;
+
+const RoadMapItem = ({ title, subtext, addToRef }) => {
+  return (
+    <Item ref={addToRef}>
+      <ItemContainer>
+        <Box>
+          <SubTitle>{title}</SubTitle>
+          <Text>{subtext}</Text>
+        </Box>
+      </ItemContainer>
+    </Item>
+  );
+};
 
 const Roadmap = () => {
-  return <div>Roadmap</div>;
+  const revealRefs = useRef([]);
+  revealRefs.current = [];
+  gsap.registerPlugin(ScrollTrigger);
+
+  const addToRefs = (el) => {
+    if (el && !revealRefs.current.includes(el)) {
+      revealRefs.current.push(el);
+    }
+  };
+
+  useLayoutEffect(() => {
+    let t1 = gsap.timeline();
+    revealRefs.current.forEach((el, index) => {
+      t1.fromTo(
+        el.childNodes[0],
+        {
+          y: "0",
+        },
+        {
+          y: "-30",
+
+          scrollTrigger: {
+            id: `section-${index + 1}`,
+            trigger: el,
+            start: "top center+=200px",
+            end: "bottom bottom",
+            scrub: true,
+            // markers: true
+          },
+        }
+      );
+    });
+
+    return () => {};
+  }, []);
+
+  return (
+    <Section>
+      <Title>Roadmap</Title>
+      <Container>
+        <SvgContainer>
+          <DrawSvg />
+        </SvgContainer>
+        <Items>
+          <Item>&nbsp;</Item>
+          <RoadMapItem
+            addToRef={addToRefs}
+            title="Grand Opening"
+            subtext="Lorem Ipsum Dolor Sit Amet Consectetur, Adipisicing Elit. At Repellat Placeat, Adipisicing Elit. At Repellat Placeat."
+          />
+          <RoadMapItem
+            addToRef={addToRefs}
+            title="Great Benefits"
+            subtext="Lorem Ipsum Dolor Sit Amet Consectetur, Adipisicing Elit. At Repellat Placeat, Adipisicing Elit. At Repellat Placeat."
+          />
+          <RoadMapItem
+            addToRef={addToRefs}
+            title="Early Access"
+            subtext="Lorem Ipsum Dolor Sit Amet Consectetur, Adipisicing Elit. At Repellat Placeat, Adipisicing Elit. At Repellat Placeat."
+          />
+          <RoadMapItem
+            addToRef={addToRefs}
+            title="New Merch"
+            subtext="Lorem Ipsum Dolor Sit Amet Consectetur, Adipisicing Elit. At Repellat Placeat, Adipisicing Elit. At Repellat Placeat."
+          />
+          <RoadMapItem
+            addToRef={addToRefs}
+            title="Holders Ranking"
+            subtext="Lorem Ipsum Dolor Sit Amet Consectetur, Adipisicing Elit. At Repellat Placeat, Adipisicing Elit. At Repellat Placeat."
+          />
+        </Items>
+      </Container>
+    </Section>
+  );
 };
 
 export default Roadmap;

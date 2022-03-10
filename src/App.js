daf02a22a01e7f56021974f276364f287b73450e
Marlon <Marlonnlmx@outlook.com>
2022-03-10 07:48:50 -0300

    Add ScrollToTop

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import Showcase from "./components/sections/Showcase";
 import Team from "./components/sections/Team";
 import Faq from "./components/sections/Faq";
 import Footer from "./components/Footer";
+import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
   return (
@@ -24,6 +25,7 @@ function App() {
         <Team />
         <Faq />
         <Footer />
+        <ScrollToTop />
       </ThemeProvider>
     </>
   );

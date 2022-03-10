b451ea821b7fbcae14a734198bb0a94ce878b16e
Marlon <Marlonnlmx@outlook.com>
2022-03-10 07:56:34 -0300

    Update

--- src/components/sections/Showcase.js
+++ src/components/sections/Showcase.js
@@ -129,7 +129,7 @@ const Showcase = () => {
   const Row2Ref = useRef(null);
 
   return (
-    <Section>
+    <Section id="showcase">
       <Row direction="none" ref={Row1Ref}>
         <NftItem img={img1} number={852} price={1.0} passRef={Row1Ref} />
         <NftItem img={img2} number={123} price={1.2} passRef={Row1Ref} />

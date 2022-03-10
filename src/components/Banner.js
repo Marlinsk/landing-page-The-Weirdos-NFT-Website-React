10f64c79ff0027b0d30ca17ca5c92e052bd34555
Marlon <Marlonnlmx@outlook.com>
2022-03-10 07:53:55 -0300

    Footer section finished

---
+++ src/components/Banner.js
@@ -0,0 +1,122 @@
+import React from "react";
+import styled from "styled-components";
+
+import Button from "./Button";
+
+import img1 from "../assets/Nfts/bighead.svg";
+import img2 from "../assets/Nfts/bighead-1.svg";
+import img3 from "../assets/Nfts/bighead-2.svg";
+import img4 from "../assets/Nfts/bighead-3.svg";
+import img5 from "../assets/Nfts/bighead-4.svg";
+import img6 from "../assets/Nfts/bighead-5.svg";
+
+const Section = styled.section`
+  width: 100vw;
+  height: 25rem;
+  position: relative;
+  border-top: 2px solid ${(props) => props.theme.text};
+  border-top: 2px solid ${(props) => props.theme.text};
+
+  background-color: ${(props) => `rgba(${props.theme.textRgba}, 0.9)`};
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  overflow: hidden;
+`;
+
+const ImgContainer = styled.div`
+  width: 100%;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  opacity: 0.2;
+
+  img {
+    width: 15rem;
+    height: auto;
+  }
+`;
+
+const Title = styled.h1`
+  font-size: ${(props) => props.theme.fontxxxl};
+  color: ${(props) => props.theme.body};
+  padding: 1rem 2rem;
+  z-index: 10;
+  width: 35%;
+  text-transform: capitalize;
+
+  text-shadow: 1px 1px 2px ${(props) => props.theme.text};
+`;
+
+const BtnContainer = styled.div`
+  width: 35%;
+  display: flex;
+  justify-content: flex-end;
+`;
+
+const JoinNow = styled.button`
+  display: inline-block;
+  background-color: ${(props) => props.theme.body};
+  color: ${(props) => props.theme.text};
+  outline: none;
+  border: none;
+  font-weight: 600;
+  font-size: ${(props) => props.theme.fontlg};
+  padding: 1.5rem 3rem;
+  border-radius: 50px;
+  cursor: pointer;
+  transition: all 0.2s ease;
+  position: relative;
+
+  &:hover {
+    transform: scale(0.9);
+  }
+
+  &::after {
+    content: " ";
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%) scale(0);
+    border: 2px solid ${(props) => props.theme.body};
+    width: 100%;
+    height: 100%;
+    border-radius: 50px;
+    transition: all 0.2s ease;
+  }
+
+  &:hover::after {
+    transform: translate(-50%, -50%) scale(1);
+    padding: 0.3rem;
+  }
+`;
+
+const Banner = () => {
+  return (
+    <Section>
+      <ImgContainer>
+        <img src={img1} alt="The Weirdos" />
+        <img src={img2} alt="The Weirdos" />
+        <img src={img3} alt="The Weirdos" />
+        <img src={img4} alt="The Weirdos" />
+        <img src={img5} alt="The Weirdos" />
+        <img src={img6} alt="The Weirdos" />
+      </ImgContainer>
+      <Title>
+        Join the <br /> weirdos club
+      </Title>
+      <BtnContainer>
+        <JoinNow>join now</JoinNow>
+      </BtnContainer>
+    </Section>
+  );
+};
+
+export default Banner;
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,169 @@
 import React from "react";
+import styled from "styled-components";
+import Banner from "./Banner";
+import Logo from "./Logo";
+
+import Facebook from "../Icons/Facebook";
+import Instagram from "../Icons/Instagram";
+import Twitter from "../Icons/Twitter";
+import LinkedIn from "../Icons/LinkedIn";
+
+const Section = styled.section`
+  min-height: 100vh;
+  width: 100vw;
+  background-color: ${(props) => props.theme.body};
+  position: relative;
+  color: ${(props) => props.theme.text};
+
+  display: flex;
+  flex-direction: column;
+`;
+
+const Container = styled.div`
+  width: 75%;
+  margin: 2rem auto;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  border-bottom: 1px solid ${(props) => props.theme.text};
+`;
+
+const IconList = styled.div`
+  display: flex;
+  align-items: center;
+  margin: 1rem auto;
+
+  & > * {
+    padding-right: 0.5rem;
+    transition: all 0.2s ease;
+
+    &:hover {
+      transform: scale(1.2);
+    }
+  }
+`;
+
+const Left = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const MenuItems = styled.ul`
+  list-style: none;
+  width: 50%;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-template-rows: repeat(3, 1fr);
+  grid-gap: 1rem;
+`;
+
+const Item = styled.li`
+  width: fit-content;
+  cursor: pointer;
+
+  &::after {
+    content: " ";
+    display: block;
+    width: 0%;
+    height: 2px;
+    background: ${(props) => props.theme.text};
+    transition: width 0.3s ease;
+  }
+
+  &:hover::after {
+    width: 100%;
+  }
+`;
+
+const Bottom = styled.div`
+  width: 75%;
+  margin: 0 auto;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  a {
+    text-decoration: underline;
+  }
+`;
 
 const Footer = () => {
-  return <div>Footer</div>;
+  const scrollTo = (id) => {
+    let element = document.getElementById(id);
+
+    element.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+      inline: "nearest",
+    });
+  };
+
+  return (
+    <Section>
+      <Banner />
+      <Container>
+        <Left>
+          <Logo />
+          <IconList>
+            <a
+              href="https://facebook.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Facebook />
+            </a>
+            <a
+              href="https://instagram.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Instagram />
+            </a>
+            <a
+              href="https://twitter.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Twitter />
+            </a>
+            <a
+              href="https://linkedin.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <LinkedIn />
+            </a>
+          </IconList>
+        </Left>
+        <MenuItems>
+          <Item onClick={() => scrollTo("home")}>Home</Item>
+          <Item onClick={() => scrollTo("about")}>About</Item>
+          <Item onClick={() => scrollTo("roadmap")}>Roadmap</Item>
+          <Item onClick={() => scrollTo("showcase")}>Showcase</Item>
+          <Item onClick={() => scrollTo("team")}>Team</Item>
+          <Item onClick={() => scrollTo("faq")}>Faq</Item>
+        </MenuItems>
+      </Container>
+      <Bottom>
+        <span>
+          &copy; {new Date().getFullYear()} Weirdos Club. All rights reserved.
+        </span>
+        <span>
+          Made with &#10084; by
+          <a
+            href="https://youtube.com/codebucks"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            CodeBucks
+          </a>
+        </span>
+      </Bottom>
+    </Section>
+  );
 };
 
 export default Footer;

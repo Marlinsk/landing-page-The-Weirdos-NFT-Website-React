dc4a5d7f3595457a9227053e7c6f18569de5e1d2
Marlon <Marlonnlmx@outlook.com>
2022-03-11 07:51:49 -0300

    Add Responsive

--- src/components/TypeWriterText.js
+++ src/components/TypeWriterText.js
@@ -26,6 +26,20 @@ const Title = styled.h2`
   .text-3 {
     color: red;
   }
+
+  @media (max-width: 70em) {
+    font-size: ${(props) => props.theme.fontxl};
+  }
+
+  @media (max-width: 48em) {
+    align-self: center;
+    text-align: center;
+    font-size: ${(props) => props.theme.fontxl};
+  }
+
+  @media (max-width: 40em) {
+    width: 90%;
+  }
 `;
 
 const SubTitle = styled.h3`
@@ -36,11 +50,29 @@ const SubTitle = styled.h3`
   margin-bottom: 1rem;
   width: 80%;
   align-self: flex-start;
+
+  @media (max-width: 40em) {
+    font-size: ${(props) => props.theme.fontmd};
+  }
+
+  @media (max-width: 48em) {
+    align-self: center;
+    text-align: center;
+  }
 `;
 
 const ButtonContainer = styled.div`
   width: 80%;
   align-self: flex-start;
+
+  @media (max-width: 48em) {
+    align-self: center;
+    text-align: center;
+
+    button {
+      margin: 0 auto;
+    }
+  }
 `;
 
 const TypeWriterText = () => {

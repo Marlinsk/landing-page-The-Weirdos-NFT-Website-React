85118e0ecf4a8e388f12d0048951a927a84a8818
Marlon <Marlonnlmx@outlook.com>
2022-03-12 00:01:50 -0300

    Add Responsive

--- src/components/sections/Faq.js
+++ src/components/sections/Faq.js
@@ -6,7 +6,7 @@ import Accordion from "../Accordion";
 
 const Section = styled.section`
   min-height: 100vh;
-  height: 100vh;
+  height: auto;
   width: 100vw;
   background-color: ${(props) => props.theme.text};
   position: relative;
@@ -26,6 +26,10 @@ const Title = styled.h1`
   margin: 1rem auto;
   border-bottom: 2px solid ${(props) => props.theme.cauroselColor};
   width: fit-content;
+
+  @media (max-width: 48em) {
+    font-size: ${(props) => props.theme.fontxl};
+  }
 `;
 
 const Container = styled.div`
@@ -35,10 +39,30 @@ const Container = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
+
+  @media (max-width: 64em) {
+    width: 80%;
+  }
+
+  @media (max-width: 48em) {
+    width: 90%;
+    flex-direction: column;
+  }
+
+  & > *::last-child {
+    & > *::first-child {
+      margin-top: 0;
+    }
+  }
 `;
 
 const Box = styled.div`
   width: 45%;
+
+  @media (max-width: 64em) {
+    width: 90%;
+    align-self: center;
+  }
 `;
 
 const Faq = () => {

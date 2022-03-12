75884a97263b5620a6ab44b11d4f81475827add4
Marlon <Marlonnlmx@outlook.com>
2022-03-12 00:02:10 -0300

    Add Responsive

--- src/components/sections/Showcase.js
+++ src/components/sections/Showcase.js
@@ -23,12 +23,21 @@ const Section = styled.section`
   justify-content: center;
   align-items: center;
   position: relative;
+  overflow: hidden;
 
   & > *:first-child {
     animation-duration: 20s;
+
+    @media (max-width: 30em) {
+      animation-duration: 15s;
+    }
   }
   & > *:last-child {
     animation-duration: 15s;
+
+    @media (max-width: 30em) {
+      animation-duration: 10s;
+    }
   }
 `;
 
@@ -53,6 +62,14 @@ const ImgContainer = styled.div`
 
   border-radius: 20px;
   cursor: pointer;
+
+  @media (max-width: 48em) {
+    width: 12rem;
+  }
+
+  @media (max-width: 30em) {
+    width: 10rem;
+  }
 
   img {
     width: 100%;
@@ -81,6 +98,10 @@ const Details = styled.div`
     font-size: ${(props) => props.theme.fontmd};
     color: ${(props) => props.theme.body};
     font-weight: 600;
+
+    @media (max-width: 30em) {
+      font-size: ${(props) => props.theme.fontsm};
+    }
   }
 `;
 

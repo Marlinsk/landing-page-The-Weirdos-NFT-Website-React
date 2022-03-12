e37d608ae8360780cc94085d332877f89f7b8f78
Marlon <Marlonnlmx@outlook.com>
2022-03-12 00:01:29 -0300

    Add Responsive

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -27,6 +27,14 @@ const Container = styled.div`
   align-items: center;
 
   border-bottom: 1px solid ${(props) => props.theme.text};
+
+  @media (max-width: 48em) {
+    width: 90%;
+
+    h1 {
+      font-size: ${(props) => props.theme.fontxxxl};
+    }
+  }
 `;
 
 const IconList = styled.div`
@@ -49,6 +57,10 @@ const Left = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
+
+  @media (max-width: 48em) {
+    width: 100%;
+  }
 `;
 
 const MenuItems = styled.ul`
@@ -58,6 +70,10 @@ const MenuItems = styled.ul`
   grid-template-columns: repeat(2, 1fr);
   grid-template-rows: repeat(3, 1fr);
   grid-gap: 1rem;
+
+  @media (max-width: 48em) {
+    display: none;
+  }
 `;
 
 const Item = styled.li`
@@ -87,6 +103,15 @@ const Bottom = styled.div`
 
   a {
     text-decoration: underline;
+  }
+
+  @media (max-width: 48em) {
+    flex-direction: column;
+    width: 100%;
+
+    span {
+      margin-bottom: 1rem;
+    }
   }
 `;
 

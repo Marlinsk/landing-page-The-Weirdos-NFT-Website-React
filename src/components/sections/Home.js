bb2a4a4350609c3a324151bd8cd1dd48cf81bd26
Marlon <Marlonnlmx@outlook.com>
2022-03-12 00:01:56 -0300

    Add Responsive

--- src/components/sections/Home.js
+++ src/components/sections/Home.js
@@ -20,6 +20,19 @@ const Container = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+
+  @media (max-width: 64em) {
+    width: 85%;
+  }
+  @media (max-width: 48em) {
+    flex-direction: column-reverse;
+    width: 100%;
+
+    &>*: first-child {
+      width: 100%;
+      margin-top: 2rem;
+    }
+  }
 `;
 
 const Box = styled.div`
@@ -51,6 +64,18 @@ const Round = styled.div`
     height: auto;
     animation: ${rotate} 6s linear infinite reverse;
   }
+
+  @media (max-width: 64em) {
+    width: 4rem;
+    height: 4rem;
+    left: none;
+    right: 2rem;
+    bottom: 100%;
+  }
+
+  @media (max-width: 48em) {
+    right: 1rem;
+  }
 `;
 
 const Circle = styled.span`
@@ -69,6 +94,12 @@ const Circle = styled.span`
   background-color: ${(props) => props.theme.text};
   color: ${(props) => props.theme.body};
   font-size: ${(props) => props.theme.fontxl};
+
+  @media (max-width: 64em) {
+    width: 2rem;
+    height: 2rem;
+    font-size: ${(props) => props.theme.fontlg};
+  }
 `;
 
 const Home = () => {
